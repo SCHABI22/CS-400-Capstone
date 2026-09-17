e7bdbc286ecad9a77adcf8509520d2f8ab1dd191
SCHABI22 <brockschall23@gmail.com>
2026-09-17 10:02:13 -0400

    Creating Docker and Git repo
    Created Docker container and Git repo
    Started Advisor logic

---
+++ BlackjackAdvisor/BlackjackAdvisor.py
@@ -0,0 +1,76 @@
+# Players & dealers cards, input by YOLO card detection
+playerCards = []
+dealerCards = []
+
+# The value of all player & dealer cards
+playerValue = 0
+dealerValue = 0
+
+# Extra player & dealer value. Used when player has an Ace which has value of 1 or 11
+playerLowAce = 0 
+dealerLowAce = 0
+
+# List of all cards the player & dealer has doubles of 
+playerDoubles = []
+dealerDoubles = []
+
+# Function for adding up all player & dealer cards 
+def returnValues(playerCards, dealerCards):
+    for p in playerCards:
+        if p <= 10:
+            playerValue += p
+        else:
+            if p < 14:
+                playerValue += 10
+            if p == 14:
+                playerLowAce += playerValue + 1
+                playerValue += 11
+
+    for d in dealerCards:
+        if p <= 10:
+            dealervalue += d
+        else:
+            if d < 14:
+                dealerValue += 10
+            if p == 14:
+                dealerLowAce += dealerValue + 1
+                dealerValue += 11
+
+    return playerCards, dealerCards
+
+# Function to detect a player or dealer bust, return -1 player bust, return -2 dealer bust, return 1 no bust
+def detectBust(playerValue, dealerValue):
+    if playerValue > 21:
+        playerValue = 0
+        if playerLowAce == 0:
+            return -1
+        if playerLowAce > 21:
+            playerLowAce = 0
+            return -1
+
+    if dealerValue > 21:
+        dealerValue = 0
+        if dealerLowAce == 0:
+            return -2
+        if dealerLowAce > 21:
+            dealerLowAce = 0
+            return -2
+
+    return 1
+
+#
+def detectDoubles(playerCards, dealerCards):
+    for i in playerCards:
+        for z in playerCards:
+            if (i == z) and (playerCards.index(i) != playerCards.index(z)):
+                playerDoubles.append(i)
+
+# 
+def advisor(playerValue, dealerValue, playerLowAce, dealerLowAce, playerCards, dealerCards):
+
+    if playerValue 
+                
+    
+
+
+
